ecd28b5187b368754614afa495545baef45bc689
1357310795 <1357310795@qq.com>
2025-02-03 17:12:34 +0800

    add task controller

---
+++ JboxTransfer.Server/Controllers/TaskController.cs
@@ -0,0 +1,71 @@
+﻿using JboxTransfer.Server.Modules.DataWrapper;
+using JboxTransfer.Server.Modules.Db;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace JboxTransfer.Server.Controllers
+{
+    [ApiController]
+    [Route("api/v1/task")]
+    public class TaskController : ControllerBase
+    {
+        private readonly ILogger<TaskController> _logger;
+        private readonly IMemoryCache _mcache;
+        private readonly DefaultDbContext _context;
+        public TaskController(ILogger<TaskController> logger, IMemoryCache memoryCache, DefaultDbContext context)
+        {
+            _logger = logger;
+            _mcache = memoryCache;
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("info")]
+        [Authorize]
+        public ApiResponse GetInfo([FromQuery]int id)
+        {
+            return new ApiResponse();
+        }
+
+        [HttpPost]
+        [Route("pause")]
+        [Authorize]
+        public ApiResponse Pause([FromForm]int id)
+        {
+            return new ApiResponse();
+        }
+
+        [HttpPost]
+        [Route("start")]
+        [Authorize]
+        public ApiResponse Start(int id)
+        {
+            return new ApiResponse();
+        }
+
+        [HttpPost]
+        [Route("cancel")]
+        [Authorize]
+        public ApiResponse Cancel(int id)
+        {
+            return new ApiResponse();
+        }
+
+        [HttpPost]
+        [Route("settop")]
+        [Authorize]
+        public ApiResponse SetTop(int id)
+        {
+            return new ApiResponse();
+        }
+
+        [HttpPost]
+        [Route("restart")]
+        [Authorize]
+        public ApiResponse Restart(int id, bool clearProgress = false)
+        {
+            return new ApiResponse();
+        }
+    }
+}
---
+++ JboxTransfer.Server/Controllers/TaskListController.cs
@@ -0,0 +1,81 @@
+﻿using JboxTransfer.Server.Modules.DataWrapper;
+using JboxTransfer.Server.Modules.Db;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace JboxTransfer.Server.Controllers
+{
+    [ApiController]
+    [Route("api/v1/tasklist")]
+    public class TaskListController : ControllerBase
+    {
+        private readonly ILogger<TaskListController> _logger;
+        private readonly IMemoryCache _mcache;
+        private readonly DefaultDbContext _context;
+        public TaskListController(ILogger<TaskListController> logger, IMemoryCache memoryCache, DefaultDbContext context)
+        {
+            _logger = logger;
+            _mcache = memoryCache;
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("list")]
+        [Authorize]
+        public ApiResponse List(string type = "transfering")
+        {
+            return new ApiResponse();
+        }
+
+        [HttpPost]
+        [Route("startall")]
+        [Authorize]
+        public ApiResponse StartAll()
+        {
+            return new ApiResponse();
+        }        
+        
+        [HttpPost]
+        [Route("pauseall")]
+        [Authorize]
+        public ApiResponse PauseAll()
+        {
+            return new ApiResponse();
+        }        
+
+        [HttpPost]
+        [Route("cancelall")]
+        [Authorize]
+        public ApiResponse CancelAll()
+        {
+            return new ApiResponse();
+        }
+
+        // error queue
+        [HttpPost]
+        [Route("restartall")]
+        [Authorize]
+        public ApiResponse RestartAll(bool clearProgress = false)
+        {
+            return new ApiResponse();
+        }
+
+        [HttpPost]
+        [Route("cancelallerr")]
+        [Authorize]
+        public ApiResponse CancelAllError()
+        {
+            return new ApiResponse();
+        }
+
+        // complete queue
+        [HttpPost]
+        [Route("deleteall")]
+        [Authorize]
+        public ApiResponse DeleteAll()
+        {
+            return new ApiResponse();
+        }
+    }
+}
